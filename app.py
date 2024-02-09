d2594b1ab6a90c8693da47678a69d121a2f88e36
Andy <38136612+andylebedev@users.noreply.github.com>
2024-02-09 07:42:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session, g
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from helpers import login_required
 
 # Configure application
 app = Flask(__name__)
@@ -13,7 +12,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
+# Configure to use SQLite database
 db = SQL("sqlite:///familytree.db")
 # con = sqlite3.connect("familytree.db", check_same_thread=False)
 # db = con.cursor()
