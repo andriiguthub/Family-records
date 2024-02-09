57217a15301f5c71e4fddf33bd92ce01e906a78b
Andy <38136612+andylebedev@users.noreply.github.com>
2024-02-09 07:22:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-# import os
-# import sqlite3
-
-from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+import sqlite3
+from flask import Flask, flash, redirect, render_template, request, session, g
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from datetime import date
 from helpers import login_required
 
 # Configure application
