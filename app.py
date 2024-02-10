43f32442e8dde2fca1e0aae6f9fa4d445841ef02
Andy <38136612+andylebedev@users.noreply.github.com>
2024-02-10 10:55:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, flash, redirect, render_template, request, session, g
+from flask_login import LoginManager
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from django.contrib.auth.decorators import login_required
