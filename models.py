d3b195c928e869910e1628595d2ba1ed39b47563
Andrii <38136612+andriiguthub@users.noreply.github.com>
2025-09-01 12:43:52 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,35 +1,35 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+udb = SQLAlchemy()
 
-class User(db.Model):
+class User(udb.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    hash = db.Column(db.String(120), nullable=False)
+    id = udb.Column(udb.Integer, primary_key=True)
+    username = udb.Column(udb.String(80), unique=True, nullable=False)
+    hash = udb.Column(udb.String(120), nullable=False)
 
-class Person(db.Model):
+class Person(udb.Model):
     __tablename__ = 'person'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    lastname = db.Column(db.String)
-    birth_date = db.Column(db.Date)
-    birth_place = db.Column(db.String)
-    death_date = db.Column(db.Date)
-    death_place = db.Column(db.String)
-    sex = db.Column(db.String)
+    id = udb.Column(udb.Integer, primary_key=True)
+    name = udb.Column(udb.String)
+    lastname = udb.Column(udb.String)
+    birth_date = udb.Column(udb.Date)
+    birth_place = udb.Column(udb.String)
+    death_date = udb.Column(udb.Date)
+    death_place = udb.Column(udb.String)
+    sex = udb.Column(udb.String)
 
-class Parent(db.Model):
+class Parent(udb.Model):
     __tablename__ = 'parent'
-    id = db.Column(db.Integer, primary_key=True)
-    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
-    father_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=True)
-    mother_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=True)
+    id = udb.Column(udb.Integer, primary_key=True)
+    person_id = udb.Column(udb.Integer, udb.ForeignKey('person.id'), nullable=False)
+    father_id = udb.Column(udb.Integer, udb.ForeignKey('person.id'), nullable=True)
+    mother_id = udb.Column(udb.Integer, udb.ForeignKey('person.id'), nullable=True)
 
-class Spouse(db.Model):
+class Spouse(udb.Model):
     __tablename__ = 'spouse'
-    id = db.Column(db.Integer, primary_key=True)
-    person_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
-    spouse_id = db.Column(db.Integer, db.ForeignKey('person.id'), nullable=False)
-    marriage_date = db.Column(db.Date)
-    divorce_date = db.Column(db.Date)
+    id = udb.Column(udb.Integer, primary_key=True)
+    person_id = udb.Column(udb.Integer, udb.ForeignKey('person.id'), nullable=False)
+    spouse_id = udb.Column(udb.Integer, udb.ForeignKey('person.id'), nullable=False)
+    marriage_date = udb.Column(udb.Date)
+    divorce_date = udb.Column(udb.Date)
