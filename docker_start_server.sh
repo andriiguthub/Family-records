6e2d941144114e20083bfa0acce4147f608c69c1
Andrii <38136612+andriiguthub@users.noreply.github.com>
2025-09-01 14:05:56 -0400

    Update docker_start_server.sh

--- docker_start_server.sh
+++ docker_start_server.sh
@@ -1,3 +1,3 @@
-wget https://raw.githubusercontent.com/andylebedev/Family-records/main/Dockerfile
-docker build --tag family-records . 
-docker run -d -e API_KEY=test -v /apps/family/familytree.db:/family/familytree.db -p 15000:5000 family-records
+wget https://raw.githubusercontent.com/andriiguthub/Family-records/refs/heads/main/Dockerfile
+docker build --no-cache --progress=plain --tag family-records .
+docker run -d -e API_KEY=test -v /apps/family/familytree.db:/family/familytree.db -p 80:5000 family-records
